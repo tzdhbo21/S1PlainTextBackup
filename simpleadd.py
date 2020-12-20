420d5d9e20293743c20f1958a3fd90aeae949b84
TomoeMami <trembleafterme@outlook.com>
2020-12-20 18:39:47 +0800

    add name

--- simpleadd.py
+++ simpleadd.py
@@ -7,7 +7,7 @@ import json
 import io
 import time
 
-rootdir="/root/S1PlainTextBackup/"
+rootdir="/home/ubuntu/S1PlainTextBackup/"
 with open(rootdir+'RefreshingData.json',"r",encoding='utf-8') as f:
     thdata=json.load(f)
 flag = 1
