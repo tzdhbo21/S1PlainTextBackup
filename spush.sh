44fb1f050f43b6df423adfe78434eb5c3a45f760
TomoeMami <trembleafterme@Outlook.com>
2021-06-02 17:51:14 +0800

    上传于 2021年06月02日 17:51

--- spush.sh
+++ spush.sh
@@ -2,8 +2,6 @@
 #datime=$(date "+%Y-%m-%d %H:%M")
 datime=$(date "+%Y年%m月%d日 %H:%M")
 cd ~/S1PlainTextBackup
-git add RefreshingData.json
-git commit -m "数据上传于 $datime"
 git add .
 git commit -m "上传于 $datime"
 echo "git commit: 上传于 $datime"
