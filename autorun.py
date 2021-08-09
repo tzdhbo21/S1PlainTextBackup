3b226db5ece78e50a70cc85eca5858e6424848a7
TomoeMami <trembleafterme@Outlook.com>
2021-08-09 17:30:11 +0800

    上传于 2021年08月09日 17:30

---
+++ autorun.py
@@ -0,0 +1,93 @@
+
+import requests
+from bs4 import BeautifulSoup
+import re
+import time
+import sys
+import io
+import os
+import json
+
+def parse_html(html,threadict):
+    # soup = BeautifulSoup(html,from_encoding="utf-8",features="lxml")
+    soup = BeautifulSoup(html, 'html.parser')
+    namelist = soup.find_all(name="tbody")
+    for i in namelist:
+        threadids = re.search(r'normalthread_\d{7}',str(i))
+        if(threadids):
+            levels = re.search(r'\d{1,5}</a></span>',str(i))
+            if(levels):
+                threadid = re.sub(r'normalthread_','',str(threadids.group(0)))
+                level = re.sub(r'</a></span>','',str(levels.group(0)))
+                if(int(level) > 2):
+                    lastreplytime = re.findall(r'\d{4}-\d{1,2}-\d{1,2} \d{2}:\d{2}',str(i))
+                    replytime = time.mktime(time.strptime(str(lastreplytime[1]), "%Y-%m-%d %H:%M"))
+                    threadict[threadid] = replytime
+    # replylist = soup.find_all(name="td", attrs={"class":"t_f"})
+    # replylist = soup.find_all(name='div', attrs={"class":"pcb"})
+    # # next_page = soup.find('a', attrs={'class': 'nxt'})
+    # # if next_page:
+    # #     return soupname, souptime, next_page['herf']
+    # title = soup.find_all(name='span',attrs={"id":"thread_subject"})
+    # total_page = int((re.findall(r'<span title="共 (\d+) 页">', str(soup)) + [1])[0])
+    # titles = re.sub(r'<.+?>','',str(title))
+    # titles = re.sub(r'[\]\[]','',titles)
+    # titles = re.sub(r'\|','｜',titles)
+    # titles = re.sub(r'/','／',titles)
+    # titles = re.sub(r'\\','＼',titles)
+    # titles = re.sub(r':','：',titles)
+    # titles = re.sub(r'\*','＊',titles)
+    # titles = re.sub(r'\?','？',titles)
+    # titles = re.sub(r'"','＂',titles)
+    # titles = re.sub(r'<','＜',titles)
+    # titles = re.sub(r'>','＞',titles)
+    # titles = re.sub(r'\.\.\.','…',titles)
+    # titles = '['+titles+']'
+    # return namelist,replylist,total_page,titles
+if __name__ == '__main__':
+    sys.stdout = io.TextIOWrapper(sys.stdout.buffer,encoding='utf8') #改变标准输出的默认编码
+    # # 浏览器登录后得到的cookie，也就是刚才复制的字符串
+    cookie_str1 = os.getenv('S1_COOKIE')
+    cookie_str = repr(cookie_str1)[1:-1]
+    # #把cookie字符串处理成字典，以便接下来使用
+    cookies = {}
+    for line in cookie_str.split(';'):
+        key, value = line.split('=', 1)
+        cookies[key] = value
+    # 设置请求头
+    headers = {'User-agent':'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.113 Safari/537.36'}
+    forumdict = {'外野': '75','虚拟主播区专楼':'151','游戏区':'4','漫区':'6',}
+    rootdir="./"
+    for k in forumdict.keys():
+        threadict = {}
+        for i in range(1,6):
+            RURL = 'https://bbs.saraba1st.com/2b/forum-'+forumdict[k]+'-'+str(i)+'.html'
+            s1 = requests.get(RURL, headers=headers,  cookies=cookies)
+            # s1 = requests.get(RURL, headers=headers)
+            # s1.encoding='utf-8'
+            data = s1.content
+            parse_html(data,threadict)
+        with open(rootdir+'RefreshingData.json',"r",encoding='utf-8') as f:
+            thdata=json.load(f)
+        flag = 1
+        ids = []
+        for j in range(len(thdata)):
+            ids.append(thdata[i]['id'])
+        for l in threadict.keys():
+            if l in ids:
+                thdata[ids.index(l)]['active'] = True
+            else:
+                newthread = {"id": l,"totalreply": 0,"title": "待更新","lastedit": int(threadict[l]),"category": k,"active": True}
+                thdata.append(newthread)
+        with open(rootdir+'RefreshingData.json',"w",encoding='utf-8') as f:
+                f.write(json.dumps(thdata,indent=2,ensure_ascii=False))
+    activethdata=[]
+    with open(rootdir+'RefreshingData.json',"r",encoding='utf-8') as f:
+            thdata=json.load(f)
+    for i in range(len(thdata)):
+        if(thdata[i]['active']):
+            activethdata.append(thdata[i])
+    with open(rootdir+'RefreshingData.json',"w",encoding='utf-8') as f:
+            f.write(json.dumps(activethdata,indent=2,ensure_ascii=False))
+        
+        
--- simpleadd.py
+++ simpleadd.py
@@ -6,30 +6,7 @@ import os
 import json
 import io
 import time
-import requests
-from bs4 import BeautifulSoup
-import re
-import math
 
-# sys.stdout = io.TextIOWrapper(sys.stdout.buffer,encoding='utf8') #改变标准输出的默认编码
-# # # 浏览器登录后得到的cookie，也就是刚才复制的字符串
-# cookie_str1 = r'_uab_collina=158915957090898555395583; __gads=ID=1b2e63b0116e499e:T=1589159571:S=ALNI_MbI4dT9moXx9c6JMgrLoNH-9yg_bA; _ga=GA1.2.962496872.1589159572; UM_distinctid=1764668d83e20f-06f0ba162f2728-5a301d45-e1000-1764668d83f1bd; __yjs_duid=1_c69e8ea7f941c1782fbd97570ca6fc741619333738518; CNZZDATA1260281688=697765024-1607495563-%7C1619765638; __cfduid=dbefde9764a01b6227381a595666e8d001620608470; B7Y9_2132_atlist=500945%2C142365; B7Y9_2132_saltkey=A7Y4qvAy; B7Y9_2132_lastvisit=1621323649; B7Y9_2132_auth=67165c2tAZdPjNDfRDoighmXgdtgE1vhu5Jp4uIijLhDhaKv%2BrdE6pnyhcWGsybUJrNClvWne9Xtq0U6gi8rU1NK7dE; B7Y9_2132_lastcheckfeed=523536%7C1621327259; B7Y9_2132_popadv=a%3A0%3A%7B%7D; B7Y9_2132_smile=1465D2; B7Y9_2132_yfe_in=1; B7Y9_2132_pc_size_c=0; B7Y9_2132_st_t=523536%7C1623045946%7C1952a6dc727ff722fe6148c696b32774; B7Y9_2132_forum_lastvisit=D_148_1616482414D_27_1616482416D_87_1617269275D_77_1618191010D_50_1621578960D_135_1622422016D_6_1623026582D_4_1623026647D_75_1623045915D_51_1623045930D_151_1623045946; B7Y9_2132_sid=UZLWBz; B7Y9_2132_lip=183.64.171.98%2C1623049646; B7Y9_2132_ulastactivity=54bfh2C9pvtAziRGDTSH6oAw4HpSqK1ExTQlHZYRFT315JJqExVi; B7Y9_2132_noticeTitle=1; B7Y9_2132_st_p=523536%7C1623054651%7Cc32ad04a12cab6165bcedf092ddee51c; B7Y9_2132_visitedfid=151D75D51D4D6D135D74D50D48D27; B7Y9_2132_viewid=tid_2007615; B7Y9_2132_lastact=1623054651%09index.php%09viewthread'
-# cookie_str = repr(cookie_str1)[1:-1]
-# # #把cookie字符串处理成字典，以便接下来使用
-# cookies = {}
-# for line in cookie_str.split(';'):
-#     key, value = line.split('=', 1)
-#     cookies[key] = value
-# # 设置请求头
-# headers = {'User-agent':'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.113 Safari/537.36'}
-# forumlist = {'外野': 75,'虚拟主播':151,'游戏':4,'动漫':6,}
-# for i in range(1,50):
-#     RURL = 'https://bbs.saraba1st.com/2b/forum-75-.html'+i+'.html'
-#     s1 = requests.get(RURL, headers=headers,  cookies=cookies)
-#     # s1 = requests.get(RURL, headers=headers)
-#     # s1.encoding='utf-8'
-#     data = s1.content
-#     namelist, replylist,totalpage,newtitles= parse_html(data)
 
 rootdir="/home/riko/S1PlainTextBackup/"
 with open(rootdir+'RefreshingData.json',"r",encoding='utf-8') as f:
